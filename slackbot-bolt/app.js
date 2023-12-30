cfbd27e4188aef15d8ebe492fe3a43c8b6cf87b8
Zach <gitlab@zingbretsen.com>
2023-12-29 22:56:27 -0500

    initial commit

---
+++ slackbot-bolt/app.js
@@ -0,0 +1,27 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+app.command('/ping', async ({ ack, say }) => {
+  // Acknowledge command request
+  await ack();
+
+  // Your code here
+  await say('pong');
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
